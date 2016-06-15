b9db8d1c7c6498ee21b56b427b88cdee763367e1
Jayanta Kurumathur <kjayanta@thoughtworks.com>
2016-06-15 12:04:46 +0530

    <Jayanta> | Implements return functionality

--- src/biblioteca/library/Library.java
+++ src/biblioteca/library/Library.java
@@ -10,16 +10,21 @@ import java.util.List;
 public class Library {
 
     private BookList inventory;
-    private BookList checkedOut = new BookList();
     private LibraryIO libraryIO;
+    private BookList checkedOut;
 
-    public Library(BookList inventory, LibraryIO libraryIO) {
+    public Library(BookList inventory, LibraryIO libraryIO, BookList checkedOut) {
         this.inventory = inventory;
         this.libraryIO = libraryIO;
+        this.checkedOut = checkedOut;
+    }
+
+    public Library(BookList inventory, LibraryIO libraryIO) {
+        this(inventory, libraryIO, new BookList());
     }
 
     Library(BookList bookList) {
-        this(bookList, new ConsoleIO());
+        this(bookList, new ConsoleIO(), new BookList());
     }
 
     BookList bookList() {
@@ -28,7 +33,7 @@ public class Library {
 
     private void mainMenu() {
         boolean restart = true;
-        List<String> menuItems = Arrays.asList("Exit", "List Books", "Checkout Book");
+        List<String> menuItems = Arrays.asList("Exit", "List Books", "Checkout Book", "Return Book");
         while (restart) {
             int choice = libraryIO.mainMenu(menuItems);
             restart = execute(choice);
@@ -45,6 +50,10 @@ public class Library {
         }
         if (choice == 2) {
             checkout();
+            return true;
+        }
+        if (choice == 3) {
+            returnBook();
             return true;
         }
         libraryIO.invalidOption();
@@ -74,21 +83,38 @@ public class Library {
         mainMenu();
     }
 
-    public void checkout() {
+    private void checkout() {
         libraryIO.display("Enter title of book to checkout: ");
-        String booktitle = libraryIO.inputBookTitle();
-        Book foundBook = inventory.findBookByName(booktitle);
+        String bookTitle = libraryIO.inputBookTitle();
+        Book foundBook = inventory.findBookByName(bookTitle);
         if (foundBook != null) {
             inventory.remove(foundBook);
             checkedOut.add(foundBook);
             libraryIO.display("Thank you! Enjoy the book!");
             return;
         }
-        foundBook = checkedOut.findBookByName(booktitle);
-        if(foundBook != null){
+        foundBook = checkedOut.findBookByName(bookTitle);
+        if (foundBook != null) {
             libraryIO.display("Sorry that book is checked out!");
             return;
         }
-        libraryIO.display("Sorry that book does not exist");
+        libraryIO.display("Sorry that book does not exist!");
+    }
+
+    private void returnBook() {
+        libraryIO.display("Enter title of book to return: ");
+        String bookTitle = libraryIO.inputBookTitle();
+        Book foundBook = checkedOut.findBookByName(bookTitle);
+        if (foundBook != null) {
+            checkedOut.remove(foundBook);
+            inventory.add(foundBook);
+            libraryIO.display("Thank you for returning the book!");
+        }
+        foundBook = inventory.findBookByName(bookTitle);
+        if(foundBook != null) {
+            libraryIO.display("That book has not been checked out!");
+            return;
+        }
+        libraryIO.display("Sorry that book does not exist!");
     }
 }
