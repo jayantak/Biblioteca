3c2df65b580fb63c0122be71c157c3bad565a7eb
Jayanta Kurumathur <kjayanta@thoughtworks.com>
2016-06-17 17:45:57 +0530

    <Jayanta> | Implements Log In option

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,7 +1,4 @@
-import biblioteca.io.ConsoleIO;
-import biblioteca.io.LibraryDataInput;
-import biblioteca.io.LibraryTextFileItemsInput;
-import biblioteca.io.UserIO;
+import biblioteca.io.*;
 import biblioteca.library.LendableList;
 import biblioteca.library.Library;
 import biblioteca.library.menuFunctions.MenuInput;
@@ -29,12 +26,19 @@ class Biblioteca {
         Library library = createLibrary();
 
         UserIO userIO = createUserIO();
-        List<User> userList = new ArrayList<>();
-        UserAuthenticator userAuthenticator = new UserAuthenticator(userList);
+        UserAuthenticator userAuthenticator = createUserAuthenticator();
 
         Menu menu = createMenu(userIO, library, userAuthenticator);
 
         menu.run();
+    }
+
+    private UserAuthenticator createUserAuthenticator() throws FileNotFoundException {
+        List<User> userList = new ArrayList<>();
+        String pathname = System.getProperty("user.dir") + libraryName + "libraryMembers.users";
+        BufferedReader userDataReader = new BufferedReader(new FileReader(pathname));
+        LibraryTextFileUsersInput libraryTextFileUsersInput = new LibraryTextFileUsersInput(userDataReader);
+        return new UserAuthenticator(libraryTextFileUsersInput.getUserList());
     }
 
     private UserIO createUserIO() {
@@ -57,6 +61,7 @@ class Biblioteca {
         MenuInput menuInput = new MenuInput(userIO);
         List<MenuOption> options = new ArrayList<>();
         options.add(new Exit());
+        options.add(new Login(userIO, userAuthenticator));
         options.add(new ListBooks(userIO, library));
         options.add(new ListMovies(userIO, library));
         options.add(new CheckoutBook(userIO, library, userAuthenticator));
